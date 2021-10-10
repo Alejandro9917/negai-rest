8161a558b4f508f82238c9f0d03265898e0e44b5
Alejandro9917 <amelendez2>
2021-10-10 14:10:26 -0600

    Pequeñas modificaciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            /*UserSeeder::class,
-            TagSeeder::class,
+            UserSeeder::class,
+            /*TagSeeder::class,
             CollectionSeeder::class,
             ComicSeeder::class,*/
         ]);
