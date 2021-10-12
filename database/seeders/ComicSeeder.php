b182345663d2878099ed6cb9fa0afb90a83eedcf
Alejandro9917 <amelendez2>
2021-10-11 19:25:03 -0600

    Arreglando el seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -18,7 +18,7 @@ class ComicSeeder extends Seeder
         DB::table('comics')->insert([
             'tag_id' => 1,
             'collection_id' => 1,
-            'name' => 'Tomo 1: Land of Lustros',
+            'name' => 'Tomo 1:',
             'author' => 'Haruko Ichikawa',
             'price' => '19.99',
             'description' => 'Sinopsis de Houseki no Kuni',
@@ -28,21 +28,5 @@ class ComicSeeder extends Seeder
             'image' => 'url imagen',
             'state' => '1',
         ]);
-
-        for ($i=0; $i < 10; $i++) { 
-            DB::table('comics')->insert([
-                'tag_id' => 1,
-                'collection_id' => 1,
-                'name' => Str::random(10),
-                'author' => Str::random(10),
-                'price' => Str::random(10),
-                'description' => Str::random(10),
-                'publisher' => Str::random(10),
-                'type' => Str::random(10),
-                'edition' => Str::random(10),
-                'image' => Str::random(10),
-                'state' => '1',
-            ]);
-        }
     }
 }
